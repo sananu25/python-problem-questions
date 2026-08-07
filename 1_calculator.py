c24d0a55c90b5e897981d671258dd8663d5c2096
Santanu <santanu7299@gmail.com>
2026-08-07 07:12:20 +0530

    Add files via upload

---
+++ 1_calculator.py
@@ -0,0 +1,15 @@
+print("enter two number ")
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+
+addition=a+b
+print("additio is :",addition)
+
+subtraction=a-b
+print("subtraction is:",subtraction)
+
+mul=a*b
+
+print("multiplication is :",mul)
+
+print("division is:",a/b)
